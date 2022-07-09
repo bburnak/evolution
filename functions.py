6bec2d11fbda02c6efdefed207d16765416738ec
bburnak <barisburnak@gmail.com>
2022-07-08 22:19:38 -0500

    Hunting skills added.
    Signed-off-by: bburnak <barisburnak@gmail.com>

--- functions.py
+++ functions.py
@@ -17,21 +17,24 @@ class DNA:
                                'base_metabolism',
                                'reproduce_efficiency',
                                'range_perception',
-                               'cancer_immunity']
+                               'cancer_immunity',
+                               'chase_tendency',
+                               'cowardice',
+                               'hunting_tendency',
+                               'hunting_efficiency']
 
 def calc_loc(df):
     nLife = len(df)
     df['state_realization_move'] = np.random.random(size = (nLife,1))
     df['resting'] = df['state_realization_move'] < df['prob_rest']
-    df = chase(df)
+    df = chase_n_runaway(df)
     xvec = np.random.normal(0, size = (nLife,1))
     yvec = np.random.normal(0, size = (nLife,1))
     totalvec = np.sqrt(xvec**2 + yvec**2+1e-8)
     df['rand_xdir'] = xvec/totalvec*1e-1
     df['rand_ydir'] = yvec/totalvec*1e-1
-    df['xdir'] = df['rand_xdir'] + df['chase_xdir']
-    df['ydir'] = df['rand_ydir'] + df['chase_ydir']
-    
+    df['xdir'] = df['rand_xdir'] + df['chase_xdir'] + df['runaway_xdir']
+    df['ydir'] = df['rand_ydir'] + df['chase_ydir'] + df['runaway_ydir']
     df.loc[~df['resting'],'xloc'] = df.loc[~df['resting'],'xloc'] + df.loc[~df['resting'],'xdir']
     df.loc[~df['resting'],'yloc'] = df.loc[~df['resting'],'yloc'] + df.loc[~df['resting'],'ydir']
     df = enervate(df)
@@ -45,46 +48,47 @@ def calc_reproduction(df):
     df.loc[df['will_reproduce'], 'energy'] = df.loc[df['will_reproduce'], 'energy']/2 * df.loc[df['will_reproduce'], 'reproduce_efficiency']
     nMutatingLife = len(df[df['will_reproduce']])
     for gene in DNA_helix.mutating_genes:
-        df.loc[df['will_reproduce'],'mutation'] = np.random.normal(1, scale = 0.005, size = (nMutatingLife,1))
+        df.loc[df['will_reproduce'],'mutation'] = np.random.normal(1, scale = 0.01, size = (nMutatingLife,1))
         new_gene = df.loc[df['will_reproduce'], gene] * df.loc[df['will_reproduce'], 'mutation']
         df.loc[df['will_reproduce'], gene] = new_gene.clip(lower = 1e-8)
-    # print(df[DNA_helix.mutating_genes].median())
-    print(df['energy'])
+    print(df[DNA_helix.mutating_genes].median())
+    # print(df['energy'])
     df = pd.concat([df,df[df['will_reproduce']]])
     return df
 
-# def calc_colors(df):
-#     nLife = len(df)
-#     df['changeColor_realization'] = np.random.random(size = (nLife,1))
-#     df['will_changeColor'] = df['changeColor_realization'] < df['prob_colorChage']
-#     nChangeColor = len(df[df['will_changeColor']])
-    
-#     df.loc[df['will_changeColor'], ['colors']] += np.random.normal(size = (nChangeColor,1))*1e-2
-#     return df
-
 def calc_colors(df):
-    if 3 < len(df):
-        DNA_helix = DNA()
-        nRedDim = 3
-        DNA_PCA = PCA(n_components=nRedDim)
-        DNA_PCA.fit(df[DNA_helix.mutating_genes])
-        lifeRGB = DNA_PCA.transform(df[DNA_helix.mutating_genes])
-        lifeRGB_norm = (lifeRGB - np.amin(lifeRGB,axis=0))/(np.amax(lifeRGB,axis=0) - np.amin(lifeRGB,axis=0))
-        # df['colors'] = lifeRGB_norm[:,]
-        # print(lifeRGB_norm[:,0])
-        # print(lifeRGB_norm[:,1])
-        # print(lifeRGB_norm[:,2])
-        df['red'] = lifeRGB_norm[:,0]
-        df['green'] = lifeRGB_norm[:,1]
-        df['blue'] = lifeRGB_norm[:,2]
-        # for i in range(len(df)):
-        #     print(df.iloc[i,'colors'])
+    nLife = len(df)
+    df['changeColor_realization'] = np.random.random(size = (nLife,1))
+    df['will_changeColor'] = df['changeColor_realization'] < df['prob_colorChage']
+    nChangeColor = len(df[df['will_changeColor']])
+    df.loc[df['will_changeColor'], ['colors']] += np.random.normal(size = (nChangeColor,1))*3e-2
     return df
+
+# def calc_colors(df):
+#     if 3 < len(df):
+#         DNA_helix = DNA()
+#         nRedDim = 3
+#         DNA_PCA = PCA(n_components=nRedDim)
+#         DNA_PCA.fit(df[DNA_helix.mutating_genes])
+#         lifeRGB = DNA_PCA.transform(df[DNA_helix.mutating_genes])
+#         lifeRGB_norm = (lifeRGB - np.amin(lifeRGB,axis=0))/(np.amax(lifeRGB,axis=0) - np.amin(lifeRGB,axis=0))
+#         # df['colors'] = lifeRGB_norm[:,]
+#         # print(lifeRGB_norm[:,0])
+#         # print(lifeRGB_norm[:,1])
+#         # print(lifeRGB_norm[:,2])
+#         df['red'] = lifeRGB_norm[:,0]
+#         df['green'] = lifeRGB_norm[:,1]
+#         df['blue'] = lifeRGB_norm[:,2]
+#         # for i in range(len(df)):
+#         #     print(df.iloc[i,'colors'])
+#     return df
     
 
-def kill_outsiders(df):
-    outsiders = (df['xloc']<-10) | (10<df['xloc']) | (df['yloc']<-10) | (10<df['yloc'])
-    df = df[~outsiders]
+def handle_outsiders(df):
+    outsider_NE = (df['yloc']<-10) | (10<df['yloc'])
+    df = df[~outsider_NE]
+    df.loc[df['xloc'] < -10, 'xloc'] = 10 + (10 + df.loc[df['xloc'] < -10, 'xloc'])
+    df.loc[10 < df['xloc'], 'xloc'] = -10 + (df.loc[10 < df['xloc'], 'xloc'] - 10)
     return df
 
 def enervate(df):
@@ -97,7 +101,7 @@ def enervate(df):
     return df
 
 def get_cancer(df):
-    max_life = 100000
+    max_life = 1000000*.5
     nLife = len(df)
     cancer_realization = np.random.random(size = (nLife,1))
     df['tmp'] = cancer_realization
@@ -108,29 +112,56 @@ def get_cancer(df):
 
 
 def get_nearest_lives(df):
+    vicinity_distance = 0.0001
     nLife = len(df)
     life_dist = squareform(pdist(df[['xloc','yloc']]), checks = False)
     np.fill_diagonal(life_dist, np.inf)
     within_range = life_dist<[df['range_perception']]*nLife
     df['within_range'] = [np.where(x == True) for x in within_range]
+    within_vicinity = life_dist < vicinity_distance*nLife
+    df['within_vicinity'] = [np.where(x == True) for x in within_vicinity]
     df['nearest_life_loc'] = np.argmin(life_dist, axis = 1)
     return df
 
-def chase(df):
+def chase_n_runaway(df):
     nLife = len(df)
-    if nLife > 10:
+    if nLife > 3:
         df = get_nearest_lives(df)
         df['tmp'] = df['within_range'].str[0].str[0]    # gives index of a life within range
-        ischasing = ~df['tmp'].isna()
+        df['tmp2'] = np.random.random(size = (nLife,1))
+        ischasing = (~df['tmp'].isna()) & (df['tmp2'] < df['chase_tendency'])
         ischased = list(df.loc[ischasing, 'tmp'].astype('int'))
         chase_xdir = df.iloc[ischased]['xloc'] - df.loc[ischasing, 'xloc']
         chase_ydir = df.iloc[ischased]['yloc'] - df.loc[ischasing, 'yloc']
         dist = np.sqrt(chase_xdir**2 + chase_ydir**2+1e-8)
-        df.loc[ischasing, 'chase_xdir'] = chase_xdir/dist*1e-1
-        df.loc[ischasing, 'chase_ydir'] = chase_ydir/dist*1e-1
-        nNonChasing = len(df.loc[~ischasing])
-        df.loc[~ischasing, 'chase_xdir'] = np.random.normal(0, size = (nNonChasing,1))*1e-1
-        df.loc[~ischasing, 'chase_ydir'] = np.random.normal(0, size = (nNonChasing,1))*1e-1
+        df.loc[ischasing, 'chase_xdir'] = chase_xdir/dist*3e-2
+        df.loc[ischasing, 'chase_ydir'] = chase_ydir/dist*3e-2
+        df.loc[~ischasing, 'chase_xdir'] = 0
+        df.loc[~ischasing, 'chase_ydir'] = 0
+        
+        df['tmp2'] = np.random.random(size = (nLife,1))
+        isrunningaway = (~df['tmp'].isna()) & (df['tmp2'] < df['cowardice'])
+        isrunningawayfrom = list(df.loc[isrunningaway, 'tmp'].astype('int'))
+        runaway_xdir = df.loc[isrunningaway, 'xloc'] - df.iloc[isrunningawayfrom]['xloc']
+        runaway_ydir = df.loc[isrunningaway, 'yloc'] - df.iloc[isrunningawayfrom]['yloc']
+        dist = np.sqrt(runaway_xdir**2 + runaway_ydir**2+1e-8)
+        df.loc[isrunningaway, 'runaway_xdir'] = runaway_xdir/dist*3e-2
+        df.loc[isrunningaway, 'runaway_ydir'] = runaway_ydir/dist*3e-2
+        df.loc[~isrunningaway, 'runaway_xdir'] = 0
+        df.loc[~isrunningaway, 'runaway_ydir'] = 0
+    return df
+
+def hunting(df):
+    nLife = len(df)
+    if nLife > 2:
+        df = get_nearest_lives(df)
+        df['tmp'] = df['within_vicinity'].str[0].str[0]    # gives index of a life within range
+        df['tmp2'] = np.random.random(size = (nLife,1))
+        ishunting = (~df['tmp'].isna()) & (df['tmp2'] < df['hunting_tendency'])
+        ishunted = list(df.loc[ishunting, 'tmp'].astype('int'))
+        df.loc[ishunting, 'energy'] += df.iloc[ishunted]['energy']*df.loc[ishunting, 'hunting_efficiency']
+        df.loc[df['max_energy'] < df['energy'], 'energy'] = df.loc[df['max_energy'] < df['energy'], 'max_energy']
+        df.iloc[ishunted]['will_die'] = True
     return df
 
 def terminate_mortals(df):
--- main.py
+++ main.py
@@ -16,13 +16,12 @@ fig, ax = plt.subplots(figsize = (15,15))
 scale = 5
 nSources = 12
 x, y = np.random.rand(1)*scale,np.random.rand(1)*scale
-colors = [0,0,0,1]
+# colors = [0,0,0,1]
+colors = 1
 transparency = [np.ones(1)]
-prob_reproduce = 0.005
 prob_colorChange = [np.random.rand(1)*1e-2]
 prob_rest = [0.8]
 energy = [100]
-athleticism = [0.1]
 graze_efficiency = [0.1]
 base_metabolism = [0.1]
 
@@ -34,34 +33,40 @@ for i in range(nSources):
     energy_source_mapped = Polygon(source_coordinates, color='g', alpha = 0.1)
     ax.add_patch(energy_source_mapped)
 
-sc = ax.scatter(x,y, color = colors, vmin = 0.0, vmax = 1.0)  
+sc = ax.scatter(x,y, c = colors, cmap = 'copper', vmin = 0.0, vmax = 1.0)  
 ax.set_xticks([])
 ax.set_yticks([])
 plt.xlim(-10,10)
 plt.ylim(-10,10)
 
 d = {'xloc': x, 'yloc': y, 'transparency': transparency,
-     'prob_reproduce': prob_reproduce,
+     'prob_reproduce': 0.002,
+     'colors': colors,
      'prob_colorChage': prob_colorChange,
      'prob_rest': prob_rest,
-     'athleticism': athleticism,
+     'athleticism': 0.2,
      'will_die': False,
      'energy': energy,
      'graze_efficiency': graze_efficiency,
      'max_energy': energy,
      'mutation': 0,
      'base_metabolism': base_metabolism,
-     'red': colors[0],
-     'green': colors[1],
-     'blue': colors[2],
+     # 'red': colors[0],
+     # 'green': colors[1],
+     # 'blue': colors[2],
      'energy_ratio': 1,
      'reproduce_efficiency': 0.75,
-     'range_perception': 0.2,
-     'cancer_immunity': 0.1,
+     'range_perception': 0.3,
+     'cancer_immunity': 0.01,
      'nearest_life_loc': 0 ,
-     'chase_tendency': 0.05,
+     'chase_tendency': 0.005,
      'chase_xdir': 0,
-     'chase_ydir': 0}
+     'chase_ydir': 0,
+     'runaway_xdir': 0,
+     'runaway_ydir': 0,
+     'cowardice': 0.02,
+     'hunting_tendency': 0.01,
+     'hunting_efficiency': 0.5}
 
 df = pd.DataFrame(data = d)
 
@@ -70,21 +75,20 @@ def animate(i):
     df = calc_loc(df)
     df = calc_reproduction(df)
     df = calc_colors(df)
-    df = kill_outsiders(df)
+    df = handle_outsiders(df)
     df = grazing(df)
+    df = hunting(df)
     df = get_cancer(df)
     df = terminate_mortals(df)
     
-    # df = get_nearest_lives(df)
     
     sc.set_offsets(np.c_[df['xloc'],df['yloc']])
-    # sc.set_array(df['colors'].astype('float'))
-    # print(df[['red','green','blue','energy_ratio']].astype('float'))
-    sc.set_color(df[['red','green','blue']].astype('float'))
-    # sc.set_alpha(df['energy_ratio'].astype('float'))
+    sc.set_array(df['colors'].astype('float'))
+    # sc.set_color(df[['red','green','blue']].astype('float'))
+    sc.set_alpha(df['energy_ratio'].astype('float'))
     ax.set_title('Number of life: ' + str(len(df)))
 ani = matplotlib.animation.FuncAnimation(fig, animate, 
-                                         frames = 2, interval = 2, repeat = True)
+                                         frames = 2, interval = 1, repeat = True)
 plt.show()
 
 def grazing(df):
